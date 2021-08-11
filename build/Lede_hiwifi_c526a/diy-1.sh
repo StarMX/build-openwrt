5761c63e06267a322546814dd4d9fbc4e4f6056b
yichu77 <yichu77@qq.com>
2021-08-11 08:53:52 +0800

    Update diy-1.sh

--- build/Lede_hiwifi_c526a/diy-1.sh
+++ build/Lede_hiwifi_c526a/diy-1.sh
@@ -13,7 +13,7 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom  #透明主题
 #git clone -b master https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash  #openclash出国软件
 #git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash  #clash出国软件
-git clone https://github.com/zzsj0928/luci-app-pushbot package/lean/luci-app-pushbot #全能推送
+#git clone https://github.com/zzsj0928/luci-app-pushbot package/lean/luci-app-pushbot #全能推送
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
 #git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
 #git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
