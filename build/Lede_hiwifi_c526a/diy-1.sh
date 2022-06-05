a7a893e084ae2bd1397bdd2f5811de8c0575fa47
StarMX <5440909@qq.com>
2022-06-05 17:29:03 +0800

    Update diy-1.sh

--- build/Lede_hiwifi_c526a/diy-1.sh
+++ build/Lede_hiwifi_c526a/diy-1.sh
@@ -8,4 +8,4 @@
 # 使用源码自带ShadowSocksR Plus+出国软件
 # sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
-echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
