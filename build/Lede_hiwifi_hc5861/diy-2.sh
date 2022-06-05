3d727a2e42e22cc469e02eba15a9f7c5305368af
StarMX <5440909@qq.com>
2022-06-05 22:31:39 +0800

    Update diy-2.sh

--- build/Lede_hiwifi_hc5861/diy-2.sh
+++ build/Lede_hiwifi_hc5861/diy-2.sh
@@ -2,8 +2,8 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
-# 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
-sed -i 's/192.168.1.1/10.1.2.1/g' package/base-files/files/bin/config_generate
+# 修改openwrt登陆地址
+sed -i 's/192.168.1.1/10.1.22.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='HiWifi_BBF'' package/lean/default-settings/files/zzz-default-settings
