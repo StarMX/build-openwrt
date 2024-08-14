0c57f463e8862bbcf1b1b7d5fab0f912c6de51f8
StarMX <5440909@qq.com>
2024-08-15 01:14:47 +0800

    更新 diy-1.sh

--- build/Lede_hiwifi_c526a/diy-1.sh
+++ build/Lede_hiwifi_c526a/diy-1.sh
@@ -9,6 +9,6 @@
 # sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
- sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-#  sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
