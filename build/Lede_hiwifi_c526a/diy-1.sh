2a9c2a1eb8d61ca5fcc63de684e84c1d233f5f89
Star.Z <5440909@qq.com>
2024-08-14 21:08:11 +0800

    fix

--- build/Lede_hiwifi_c526a/diy-1.sh
+++ build/Lede_hiwifi_c526a/diy-1.sh
@@ -10,5 +10,5 @@
 
 # echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+#  sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
- rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
