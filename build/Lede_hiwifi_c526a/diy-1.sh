0fe5cb9684a1ee0a3661389a184f7c927ab50beb
Star.Z <5440909@qq.com>
2024-08-15 22:42:17 +0800

    fix

--- build/Lede_hiwifi_c526a/diy-1.sh
+++ build/Lede_hiwifi_c526a/diy-1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
 # 使用自定义package
@@ -11,7 +10,7 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 
 
 # echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 
