68612d2e6f3e379eb610127c6d8d956c59b68e96
Star.Z <5440909@qq.com>
2024-08-15 22:41:54 +0800

    fix

--- build/Lede_hiwifi_c526a/diy-2.sh
+++ build/Lede_hiwifi_c526a/diy-2.sh
@@ -4,5 +4,9 @@
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
+
+if [ -d ./feeds/packages/lang/golang ]; then
+    rm -rf ./feeds/packages/lang/golang
+fi
+
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
