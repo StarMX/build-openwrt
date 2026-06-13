6a4eeadadf4971471c707992f9abacf2ea9ec5fa
StarMX <9284949+StarMX@users.noreply.github.com>
2026-06-13 12:22:18 +0800

    Update diy-1.sh

--- build/Lede_hiwifi_c526a/diy-1.sh
+++ build/Lede_hiwifi_c526a/diy-1.sh
@@ -10,7 +10,7 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 
 
 # echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 
