d149dcdcf31ebb132b3f4e44fc54899c0d60ab4e
StarMX <9284949+StarMX@users.noreply.github.com>
2026-06-13 12:23:41 +0800

    Remove luci-theme-argon from build script
    Remove luci-theme-argon from package and feeds.

--- build/Lede_hiwifi_c526a/diy-1.sh
+++ build/Lede_hiwifi_c526a/diy-1.sh
@@ -13,7 +13,8 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-
+rm -rf package/lean/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon
 
 
 
